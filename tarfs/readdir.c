39a4c1f03d7d687309f03777c7f259d44b115485
eric29200 <eric_begot@yahoo.fr>
2022-04-13 20:23:53 +0200

    TarFS : bug fix

--- tarfs/readdir.c
+++ tarfs/readdir.c
@@ -10,7 +10,7 @@ int tarfs_getdents64(struct file_t *filp, void *dirp, size_t count)
 {
   struct dirent64_t *dirent = (struct dirent64_t *) dirp;
   struct tar_entry_t *tar_entry, *child;
-  int entries_size = 0, i = 0;
+  int entries_size = 0, i = 2;
   struct list_head_t *pos;
   size_t name_len;
 
@@ -19,22 +19,11 @@ int tarfs_getdents64(struct file_t *filp, void *dirp, size_t count)
   if (!tar_entry)
     return -ENOENT;
 
-  /* add "." and ".." entries */
+  /* add ".." entry */
   if (filp->f_pos == 0) {
     /* check if input buffer is big enough */
-    if (count < sizeof(struct dirent64_t) * 2 + 2 + 3)
+    if (count < sizeof(struct dirent64_t) + 3)
       return -ENOSPC;
-
-    /* add "." entry */
-    dirent->d_inode = tar_entry->ino;
-    dirent->d_off = 0;
-    dirent->d_reclen = sizeof(struct dirent64_t) + 2;
-    dirent->d_type = 0;
-    dirent->d_name[0] = '.';
-    dirent->d_name[1] = 0;
-    count -= dirent->d_reclen;
-    entries_size += dirent->d_reclen;
-    dirent = (struct dirent64_t *) ((char *) dirent + dirent->d_reclen);
 
     /* add ".." entry */
     dirent->d_inode = tar_entry->parent ? tar_entry->parent->ino : tar_entry->ino;
@@ -47,6 +36,26 @@ int tarfs_getdents64(struct file_t *filp, void *dirp, size_t count)
     count -= dirent->d_reclen;
     entries_size += dirent->d_reclen;
     dirent = (struct dirent64_t *) ((char *) dirent + dirent->d_reclen);
+    filp->f_pos = 1;
+  }
+
+  /* add "." entry */
+  if (filp->f_pos == 1) {
+    /* check if input buffer is big enough */
+    if (count < sizeof(struct dirent64_t) + 2)
+      return -ENOSPC;
+
+    /* add "." entry */
+    dirent->d_inode = tar_entry->ino;
+    dirent->d_off = 0;
+    dirent->d_reclen = sizeof(struct dirent64_t) + 2;
+    dirent->d_type = 0;
+    dirent->d_name[0] = '.';
+    dirent->d_name[1] = 0;
+    count -= dirent->d_reclen;
+    entries_size += dirent->d_reclen;
+    dirent = (struct dirent64_t *) ((char *) dirent + dirent->d_reclen);
+    filp->f_pos = 2;
   }
 
   /* for each child */
